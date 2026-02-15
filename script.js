afa4f1a3bc8ee1a4f40ad7e6a42d6920c3384993
Rudra Sharma <rudras.1902@gmail.com>
2026-02-15 10:08:46 +0530

    changed song names for better readability

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ async function getSongs() {
     let div = document.createElement("div")
     div.innerHTML = response
     let as = div.getElementsByTagName("a")
-    let songs=[]
+    let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href)
         }
     }
     return songs
 }
 
-getSongs()
+async function main() {
+    //to get list of songs
+    let songs = await getSongs()
+    console.log(songs)
+
+    //play the first song
+    var audio = new Audio('audio_file.mp3');
+    audio.play();
+
+}
+main()
