9f65ca696cd5b55a5fcc647f676689db1526d971
Rudra Sharma <rudras.1902@gmail.com>
2026-02-15 09:59:23 +0530

    implement getSongs function to fetch and return song URLs

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("LETS GO")
+
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text()
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs
+}
+
+getSongs()
