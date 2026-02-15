dbf21d362e00efcffd00822b79e63c3e4a3431fb
Rudra Sharma <rudras.1902@gmail.com>
2026-02-15 21:43:40 +0530

    refactor song list rendering and improve CSS styles for better layout

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-console.log("LETS GO")
+console.log("LETS GO");
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/")
-    let response = await a.text()
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    let songs = []
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            // FIX: This split looks for the song name regardless of / or \ 
-            let songName = element.href.split("/songs/")[1] || element.href.split("%5Csongs%5C")[1];
-            songs.push(songName)
+            // FIX: This split looks for the song name regardless of / or \
+            let songName =
+                element.href.split("/songs/")[1] ||
+                element.href.split("%5Csongs%5C")[1];
+            songs.push(songName);
         }
     }
-    return songs
+    return songs;
 }
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:3000/songs/")
-    let response = await a.text()
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    let songs = []
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
@@ -35,22 +37,34 @@ async function getSongs() {
             }
         }
     }
-    return songs
+    return songs;
 }
 
 async function main() {
     // Get the list of all the songs
-    let songs = await getSongs()
-    console.log(songs)
+    let songs = await getSongs();
+    console.log(songs);
 
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    let songUL = document
+        .querySelector(".songList")
+        .getElementsByTagName("ul")[0];
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+        songUL.innerHTML =
+            songUL.innerHTML + `<li> 
+            <img class="invert" src="music.svg" alt="">
+                  <div class="info">
+                    <div>${song.replaceAll("%20", " ").replaceAll(".mp3", "")}</div>
+                  </div>
+                  <div class=""playnow>
+                    <span>Play Now</span>
+                    <img class="invert" src="playNow.svg" alt="">
+                  </div>
+         </li>`;
     }
 
     // Play the first song
-    // var audio = new Audio("/songs/" + songs[0]);
-    // audio.play();
+    //   var audio = new Audio("/songs/" + songs[1]);
+    //   audio.play();
 }
 
-main()
+main();
